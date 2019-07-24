1897d2603616b57177e2cffb048daf3482cc3bb1
Luke Bosse <lukebosse@u.boisestate.edu>
2019-07-23 17:49:20 -0700

    added swaparray example

---
+++ examples/Arrays/SwapArray/SwapRandomArray.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+
+public class SwapRandomArray {
+
+	private int[] randomArray;
+
+	public SwapRandomArray(int size) {
+		Random rand = new Random();
+
+		randomArray = new int[size];
+
+		for (int i = 0; i < size; i++) {
+			randomArray[i] = rand.nextInt(100);
+		}
+	}
+
+	public void swap(int i, int j) {
+		int tmp = randomArray[i];
+		randomArray[i] = randomArray[j];
+		randomArray[j] = randomArray[i];
+	}
+
+	public String toString() {
+		String arrayStr = "";
+		for (int i = 0; i < randomArray.length; i++) {
+			arrayStr += "index: " + i + ", value: " + randomArray[i] + "\n";
+		}
+		return arrayStr;
+	}
+}
---
+++ examples/Arrays/SwapArray/SwapRandomArrayDriver.java
@@ -0,0 +1,14 @@
+
+public class SwapRandomArrayDriver {
+
+	public static void main(String[] args) {
+		
+		SwapRandomArray swapArray = new SwapRandomArray(10);
+		System.out.println(swapArray);
+		
+		swapArray.swap(4, 5);
+		System.out.println(swapArray);
+		
+	}
+
+}
