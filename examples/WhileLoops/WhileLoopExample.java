08b7986ab902b87e6f9838d09cca8a1d2796f1fb
Luke Bosse <lukebosse@u.boisestate.edu>
2019-10-03 19:26:07 -0600

    added while loop example

---
+++ examples/WhileLoops/WhileLoopExample.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class WhileLoopExample {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		String codeword = "CODEISCOOL";
+
+		String playAgain = "y";
+		while (playAgain.toLowerCase().equals("y")) {
+			String input = "";
+			while (!input.equals(codeword)) {
+				System.out.print("Enter the code word!!: ");
+				input = scan.next();
+				if (!input.contentEquals(codeword))
+					System.out.println("HAHAHA! Try again... ");
+			}
+			System.out.print("Good job! Want to play again? y/n: ");
+			playAgain = scan.next();
+		}
+		System.out.println("Game over. Goodbye!");
+	}
+
+}
