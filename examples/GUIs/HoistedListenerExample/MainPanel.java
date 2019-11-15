5d9fb9646c4e3325de5b19b3523cb18dbe479d92
Luke Bosse <lukebosse@u.boisestate.edu>
2019-11-14 17:53:59 -0700

    updated

--- examples/GUIs/HoistedListenerExample/MainPanel.java
+++ examples/GUIs/HoistedListenerExample/MainPanel.java
@@ -7,15 +7,16 @@ import javax.swing.JButton;
 import javax.swing.JPanel;
 
 public class MainPanel extends JPanel {
+	// It's important to have the DisplayPanel as an instance variable so
+	// that we can access it in the listener.
 	private DisplayPanel dp;
-	private ButtonPanel bp;
 	
 	public MainPanel() {
 		this.setBackground(Color.GRAY);
 		this.setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
 		
 		dp = new DisplayPanel();
-		bp = new ButtonPanel(new ButtonListener());
+		ButtonPanel bp = new ButtonPanel(new ButtonListener());
 		
 		this.add(dp);
 		this.add(bp);
@@ -25,6 +26,8 @@ public class MainPanel extends JPanel {
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
+			// Call the DisplayPanel's setText method to set the TextArea
+			// text to the text of the clicked button
 			dp.setText(((JButton)e.getSource()).getText());
 		}
 		
